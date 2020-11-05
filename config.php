806f817569219820fb169fc7444a367ef3f445a7
isaelsilva1039 <71187451+isaelsilva1039@users.noreply.github.com>
2020-11-05 16:41:55 -0300

    oficina

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$nome_oficina = "Oficina Isael Duarte";
+$url = "http://localhost/oficina/painel-adm/";
+$endereco_oficina = "A.neiva de moreirera";
+$telefone_oficina = "(99)992292338";
+$email_adm = "isaelsilva1039@gmail.com";
+
+
+$servidor = 'localhost';
+$usuario  = 'root';
+$senha    = '';
+$banco    ='oficina';
+
+
+
+?>
